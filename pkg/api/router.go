b3a7ebe5c76a9d577bc72e92115548c33d1b6e78
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2023-08-02 23:55:28 +0530

    Unable to serve static files

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -11,6 +11,8 @@ import (
 func Start() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controller.Home).Methods("GET")
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	http.ListenAndServe(":8000", r)
 }
