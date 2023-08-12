ff85f644274ff354127ed031a0ad66290e387365
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2023-08-13 05:12:22 +0530

    Update readme; minor changes in bash scripts;

--- scripts/dbSetup.sh
+++ scripts/dbSetup.sh
@@ -6,4 +6,16 @@ echo
 
 DB_NAME="librago"
 
-migrate -path database/migration/ -database "mysql://${sqlUsername}:${dbPassword}@tcp(localhost:3306)/${DB_NAME}" -verbose up
+mysql -u "$sqlUsername" -p"$dbPassword" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+
+migrate -path ../database/migration/ -database "mysql://${sqlUsername}:${dbPassword}@tcp(localhost:3306)/${DB_NAME}" -verbose up
+
+read -p "Enter admin password" adminPassword
+
+cat <<EOF > ../config.yaml
+DB_USERNAME: $sqlUsername
+DB_PASSWORD: $dbPassword
+DB_HOST: "127.0.0.1:3306"
+DB_NAME: "librago"
+JWT_SECRET: $adminPassword
+EOF
